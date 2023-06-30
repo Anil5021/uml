b6385769413e9e5b4fa1488acddb5802993e3b19
Anil-23May2017 <adepu.anil2@gmail.com>
2023-06-30 16:55:31 +0530

    First Project in intellij

---
+++ src/Test.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Test {
+    public static void main(String[] args) {
+        // Press Alt+Enter with your caret at the highlighted text to see how
+        // IntelliJ IDEA suggests fixing it.
+        System.out.println("Hello Sanvi and welcome!");
+        for (int i = 0; i < 5; i++) {
+            System.out.println("I" + i);
+        }
+        List<Integer> list = new ArrayList<Integer>();
+        Map<String, Integer> map = new HashMap<String, Integer>();
+    }
+}
